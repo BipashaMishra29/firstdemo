88a89c561c20e023ad1e88b28d487acff8e46ae4
BipashaMishra29 <bipashamishra30@gmail.com>
2023-05-26 13:05:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstdemo';
+  name='puja'
+  college='IEM'
+  degree='BCA'
+  passout=2023
+  arr=["amitava","iosajah","undefined"];
+  products=[
+    {
+      brand:"SONY",
+      price:40000,
+      screensize:43
+    
+    },
+    
+      
+    {
+      brand:"SAMSUNG",
+      price:42000,
+      screensize:50
+    },
+    {
+      brand:"LG",
+      price:50000,
+      screensize:55
+    },
+    {
+      brand:"MI",
+      price:10000,
+      screensize:33
+    },
+    {
+      brand:"Philips",
+      price:35000,
+      screensize:63
+    }
+  ];
+  fun1()
+  {
+    console.log('HI I AM ANGULAR FUNTION!!!')
+    alert('I am a function')
+  console.log(this.products[0].brand)
+  }
+  
+
+  
+
+  
+
+
+
+
+
 }
